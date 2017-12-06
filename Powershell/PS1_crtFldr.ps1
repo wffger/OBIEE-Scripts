1ae76b1e293e85d2e69f6e86eea4b673e751371e
wffger <wffger@live.cn>
2017-12-06 19:59:04 +0800

    Initial commit

---
+++ Powershell/PS1_crtFldr.ps1
@@ -0,0 +1,11 @@
+﻿############################### 
+# (C) 2017 wffger 
+# https://github.com/wffger
+# 
+# 此脚本每天创建文件夹
+###############################
+$today = Get-date
+$srcFldr="D:\BI-bak\" 
+$srcFldr=$srcFldr+$today.ToString('yyyyMMdd')
+
+New-Item -Path $srcFldr -ItemType directory -Force | out-null
---
+++ Powershell/PS4_cpyFldr.ps1
@@ -0,0 +1,17 @@
+﻿############################### 
+# (C) 2017 wffger 
+# https://github.com/wffger
+# 
+# 此脚本复制文件夹到远程目录
+###############################
+$srcFldr="D:\BI-bak\"
+$trgFldr="\\10.0.100.100\bi项目实施\备份"
+
+$today = Get-date
+$srcFldr=$srcFldr+$today.ToString('yyyyMMdd')
+
+###############################
+Write-Host "复制${srcFldr}到${trgFldr}……"
+Copy-Item -recurse $srcFldr $trgFldr
+Write-Host "复制完成，按回车键结束。"
+$null = [System.Console]::ReadKey()
